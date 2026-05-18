a665c7bd658fd9b985aa41569ffe5603517b40f0
Marsha Teo <mateo@lab3r1s14.42abudhabi.ae>
2026-05-18 11:24:24 +0400

    add ex04 for 0-starting

---
+++ 0-starting/ex04/whatis.py
@@ -0,0 +1,28 @@
+import sys
+
+def is_even(number):
+    if number % 2 == 0:
+        print("I'm Even.")
+    else:
+        print("I'm Odd.")
+
+def parse_argument():
+    if len(sys.argv) > 2:
+        raise AssertionError("more than one argument is provided")
+    elif len(sys.argv) == 1:
+        return None
+    try:
+        return int(sys.argv[1])
+    except ValueError:
+        raise AssertionError("argument is not an integer")
+
+def main():
+    try:
+        number = parse_argument()
+        if number is not None:
+            is_even(number)
+    except AssertionError as e: 
+        print(f"AssertionError: {e}")
+
+if __name__ == "__main__":
+    main()
