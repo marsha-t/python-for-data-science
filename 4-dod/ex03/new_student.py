54784ec64d36389ad88d02f875d21f7d961001a6
Marsha <151203981+marsha-t@users.noreply.github.com>
2026-06-03 11:46:01 +0400

    add ex03

---
+++ 4-dod/ex03/new_student.py
@@ -0,0 +1,19 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+def generate_id() -> str:
+	return "".join(random.choices(string.ascii_lowercase, k = 15))
+
+@dataclass
+class Student:
+	"""Represent a student with generated login and ID"""
+	name: str
+	surname: str # TODO what if surname not given fully in lowercase
+	active: bool = True
+	login: str = field(init=False)
+	id: str = field(init=False, default_factory=generate_id)
+
+	def __post_init__(self) -> None:
+		"""Create student login after initialisation"""
+		self.login = self.name[0].upper() + self.surname.lower()
